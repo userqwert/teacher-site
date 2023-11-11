90d81b842f768a1c1b37f687c68ce18c17909b7a
Pasturь_Utka <vladlokki@gmail.com>
2023-11-11 17:19:43 +0200

    ch5

--- django_project/settings.py
+++ django_project/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     # Local
     "accounts.apps.AccountsConfig",
+    "pages.apps.PagesConfig",
 ]
 
 AUTH_USER_MODEL = "accounts.CustomUser"
@@ -58,7 +59,7 @@ ROOT_URLCONF = "django_project.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
